247b3f4edf9672ea60abd6228fe30ee7b4a064b4
amnbot <aymenouali01@gmail.com>
2023-11-17 20:09:03 -0500

    Add initial files and configurations for CodeJam13 project

---
+++ codejam13/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="">
+      <Header />
+      <Sidebar />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Dashboard />} />
+        </Routes>
+        {/* <Footer /> */}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
