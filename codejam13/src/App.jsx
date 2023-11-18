8b4c8365635945773f7733704a3159832db49c14
amnbot <aymenouali01@gmail.com>
2023-11-18 15:46:10 -0500

    Refactor question card component and add submit button

--- codejam13/src/App.jsx
+++ codejam13/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
           <Route element={<PrivateRoute />}>
             <Route path="/" element={<Dashboard />} exact/>
             <Route path="/create-exam" element={<CreateExam exact/>} /> 
-            <Route path="/my-exams" element={<MyExams />} exact/>
+            <Route path="/my-exams/:id" element={<MyExams />} exact/>
           </Route>
         </Routes>
         {/* <Footer /> */}
