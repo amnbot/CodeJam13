e2a93231ea8e0c00df4ee2791395aec4af182aff
Anto1Roy <antonin1roy@hotmail.com>
2023-11-18 17:06:06 -0500

    pullin them in

---
+++ codejam13/src/components/AlignListItem.jsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+
+const AlignItemsList = (members) => {
+  return (
+    <List sx={{ width: "100%", padding: "10px", bgcolor: "background.paper" }}>
+      {members.items.map((member) => (
+        <>
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary={member.name}
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    {member.email}
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+        </>
+      ))}
+      ;
+    </List>
+  );
+};
+export default AlignItemsList;
