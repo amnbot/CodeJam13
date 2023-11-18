82716af7ff9062e015093871c6896688086d5fa6
RadhSlayer <57155052+RadhSlayer@users.noreply.github.com>
2023-11-18 11:43:34 -0500

    firestore bhays

---
+++ codejam13/src/utils/firestoreConfigSetter.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { firebaseConfig } from "./firebaseConfig";
+
+
+export const app = initializeApp(firebaseConfig);
+
+export const provider = new GoogleAuthProvider();
+
+// whenever a user interacts with the provider, we force them to select an account
+provider.setCustomParameters({
+  prompt: "select_account ",
+});
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
---
+++ codejam13/src/utils/firestoreFunctions.js
@@ -0,0 +1,40 @@
+import {db} from "../firestoreConfigSetter.js";
+import { collection, addDoc, getDoc, updateDoc, query, where, arrayUnion, arrayRemove} from "firebase/firestore";
+
+const addExam = async (exam) =>{
+    const examRef = await addDoc(collection(db, "exams"), exam);
+    return examRef.id;
+
+}
+
+const getExam = async (id) =>{
+  const examRef = collection(db, "exams");
+  const q = query(examRef, where("id", "==", id));
+  const doc = await getDoc(q);
+
+  return doc.data();
+}
+
+const updateExamName = async(id, newName) => {
+    const examRef = collection(db, "exams");
+    const q = query(examRef, where("id", "==", id));
+    const doc = await getDoc(q);
+
+    await updateDoc(doc, {name :newNname });
+}
+
+const addExamQuestions = async(id, newQ) => {
+    const examRef = collection(db, "exams");
+    const q = query(examRef, where("id", "==", id));
+    const doc = await getDoc(q);
+
+    await updateDoc(doc, {questions: arrayUnion(newQ)});
+}
+
+const removeExamQuestions = async(id, oldQ) =>{
+    const examRef = collection(db, "exams");
+    const q = query(examRef, where("id", "==", id));
+    const doc = await getDoc(q);
+
+    await updateDoc(doc, {questions: arrayRemove(oldQ)});
+}
