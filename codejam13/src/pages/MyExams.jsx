0207c987182da25e33eacc5b0859eacc1c8c3f98
RadhSlayer <57155052+RadhSlayer@users.noreply.github.com>
2023-11-18 20:09:48 -0500

    bugfix

--- codejam13/src/pages/MyExams.jsx
+++ codejam13/src/pages/MyExams.jsx
@@ -97,14 +97,7 @@ export default function MyExams() {
           >
             Search
           </button>
-
-          <button
-            className="relative z-[2] rounded-r border-2 border-primary px-6 py-2 text-xs font-medium uppercase text-primary transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
-            type="button"
-            id="button-add"
-          >
-            +
-          </button>
+          
         </div>
 
         <div>
