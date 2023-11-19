6268d03854797ad06ff057365be3bf451867764d
amnbot <aymenouali01@gmail.com>
2023-11-19 11:04:55 -0500

    radio button size

--- codejam13/src/components/QuestionCardMCQ.jsx
+++ codejam13/src/components/QuestionCardMCQ.jsx
@@ -15,16 +15,16 @@ const QuestionCardMCQ = ({
   return (
     <div className="bg-gray-900 focus:border-blue-500 p-4 rounded-3xl">
       <div className="justify-between m-auto my-4">
-        <h3 className="font-bold justify-start">
+        <h3 className="font-bold justify-start text-xl">
           Question {questionIndex + 1}
         </h3>
-        <h3 className="italic justify-end">{question}</h3>
+        <h3 className="italic justify-end text-2xl">{question}</h3>
       </div>
       <div className="text-left">
         {options.map((choice, choiceIndex) => (
           <div key={choiceIndex}>
             <input
-              className="outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+              className="transform scale-150 outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
               type="radio"
               id={`choice-${choiceIndex}`}
               name="answer"
@@ -38,7 +38,7 @@ const QuestionCardMCQ = ({
                 });
               }}
             />
-            <label htmlFor={`choice-${choiceIndex}`}>{choice}</label>
+            <label className="mx-2 text-xl" htmlFor={`choice-${choiceIndex}`}>{choice}</label>
           </div>
         ))}
       </div>
