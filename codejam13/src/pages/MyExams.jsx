efe4d6f0219f3acee6c7486416c61d5d7f6971bf
RadhSlayer <57155052+RadhSlayer@users.noreply.github.com>
2023-11-18 20:08:47 -0500

    flashcards

--- codejam13/src/pages/MyExams.jsx
+++ codejam13/src/pages/MyExams.jsx
@@ -10,6 +10,22 @@ import {
 } from "../utils/firestoreFunctions";
 import CardSingle from "./CardSingle";
 import { cardActionAreaClasses } from "@mui/material";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const modalStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function MyExams() {
   let { id } = useParams();
@@ -107,11 +123,27 @@ export default function MyExams() {
           >
             {exam.multipleChoice.map((questionData, index) => (
               <Grid item xs={2} sm={4} md={4} key={index}>
-                <p>{questionData.question}</p>
+                <button onClick={() => questionButton(questionData)}>{questionData.question}</button>
               </Grid>
             ))}
           </Grid>
         </div>
+
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={modalStyle}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              {modalQ}
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              {modalA}
+            </Typography>
+          </Box>
+        </Modal>
       </div>
     );
   }
